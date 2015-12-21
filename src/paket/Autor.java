f32c71f13dbbb131c0900426be4f47317d2ef73c
student <student@ru519.bmu.internal>
2015-12-21 11:56:32 +0100

    gospode komituj

--- src/paket/Autor.java
+++ src/paket/Autor.java
@@ -2,4 +2,33 @@ package paket;
 
 public class Autor {
 
+	private String ime;
+	private String prezime;
+
+	public Autor() {
+		super();
+	}
+
+	public Autor(String ime, String prezime) {
+		super();
+		this.ime = ime;
+		this.prezime = prezime;
+	}
+
+	public String getIme() {
+
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
 }
