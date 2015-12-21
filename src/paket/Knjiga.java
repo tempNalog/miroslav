f1353bc48f5f098304bf630cc0fb3ee10267a254
student <student@ru519.bmu.internal>
2015-12-21 10:47:07 +0100

    dirao knjigu

--- src/paket/Knjiga.java
+++ src/paket/Knjiga.java
@@ -2,4 +2,58 @@ package paket;
 
 public class Knjiga {
 
+	private String naslov;
+	private long isbn;
+	private String izdavac;
+	private int izdanje;
+
+	public Knjiga() {
+		super();
+	}
+
+	public Knjiga(String naslov, long isbn, String izdavac, int izdanje) {
+		super();
+		this.naslov = naslov;
+		this.isbn = isbn;
+		this.izdavac = izdavac;
+		this.izdanje = izdanje;
+	}
+
+	public String getNaslov() {
+		return naslov;
+	}
+
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getIzdavac() {
+		return izdavac;
+	}
+
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+
+	public int getIzdanje() {
+		return izdanje;
+	}
+
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", izdavac="
+				+ izdavac + ", izdanje=" + izdanje + "]";
+	}
 }
